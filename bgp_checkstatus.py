c1d3d852cd0e014a70ad9253b33aba73e376d4f9
prabin <52509978+mhprabin@users.noreply.github.com>
2020-04-10 22:40:33 -0400

    Update bgp_checkstatus.py

--- bgp_checkstatus.py
+++ bgp_checkstatus.py
@@ -9,5 +9,5 @@ with open('bgp_sum') as device_info:
         device_info['Neighbor'] = this_dev[0]
         device_info['State'] = this_dev[8]
         #print (this_dev)
-        if this_dev[8] == 'Active':
+        if this_dev[8] == 'Active': # we can add multiple statement here. for testing only i'm using 'Active'
             print ('BGP neighbor ' + device_info['Neighbor'] + ' is down')
