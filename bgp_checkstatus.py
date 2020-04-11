e699d30d02e7d33826c955b92a53955266e24bc5
prabin <52509978+mhprabin@users.noreply.github.com>
2020-04-10 22:39:44 -0400

    Create bgp_checkstatus.py

---
+++ bgp_checkstatus.py
@@ -0,0 +1,13 @@
+import re
+
+with open('bgp_sum') as device_info:
+    for lines in device_info:
+        this_dev1 = re.sub(' +', ' ',lines)
+        #print (this_dev1)
+        this_dev = this_dev1.strip().split(' ')
+        device_info ={}
+        device_info['Neighbor'] = this_dev[0]
+        device_info['State'] = this_dev[8]
+        #print (this_dev)
+        if this_dev[8] == 'Active':
+            print ('BGP neighbor ' + device_info['Neighbor'] + ' is down')
